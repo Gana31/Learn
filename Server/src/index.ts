264fe4a5ab3be4a65a5a4646115fd1aef9a00d66
Gana31 <ganeshronghe2@gmail.com>
2024-07-20 20:51:56 +0530

    add vercel with update

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -34,7 +34,7 @@ api_key : process.env.CLOUD_API_KEY,
 api_secret : process.env.CLOUD_SECRET_KEY,
 })
 app.get("/",(req: Request,res:Response , next : NextFunction)=>{
-  res.send("hello")
+  res.send("hello");
     })
 
 
